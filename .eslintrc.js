ea5503da7510165d8c473d8e92c5da46f4380836
howzy <ihowzy@gmail.com>
2018-06-22 01:43:01 +0800

    初始化项目

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  extends: ['plugin:vue/essential', 'plugin:prettier/recommended'],
+  // required to lint *.vue files
+  plugins: [
+    'vue',
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': ['error', {
+      'singleQuote': true,
+      'trailingComma': false,
+      'semi': false
+    }]
+  }
+}
